a5f4cb473fee296e155c6b48340230635e9f6e9d
FoxGeoff <FoxGeoff@users.noreply.github.com>
2020-01-09 15:42:47 -0800

    Fix Error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products/products.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'products', component: ProductComponent}
+  {path: 'products', component: ProductsComponent}
 ];
 
 @NgModule({
